e77cc6484f32e8f5415e2569761aec782ad62224
wiktor <wiktor@wiktor.chello.pl>
2013-08-17 16:29:05 +0200

    Make adjustment...

--- make_gmm_final.sh
+++ make_gmm_final.sh
@@ -9,7 +9,9 @@ rm gmm_final.exe
 #gcc gmm_final_001.c -Wall -ansi -pedantic -lgsl -lgslcblas -lm -o gmm_final.exe
 #
 gcc gmm_final_002.c -Wall -ansi -pedantic -lgsl -lgslcblas -lm -o gmm_final.exe
-./gmm_final.exe # > data_gmm_2_s_b_r_kappa_0_0_s_1026_tf_800_H_A.dat 
+#./gmm_final.exe # > data_gmm_2_s_b_r_kappa_0_0_s_1026_tf_800_H_A.dat 
+# run the soft and save to file:
+./gmm_final.exe
 #  
 #  data_gmm_3_s_a_r_kappa_0_0_s_1026_H_A.dat #(H_A odpowiednik pliku data_gmm_3_s_a_r_kappa_0_0_s_1026.dat)
 #  data_gmm_2_s_a_r_kappa_0_0_s_1026_H_A.dat #(H_A odpowiednik pliku data_gmm_2_s_a_r_kappa_0_0_s_1026.dat)
